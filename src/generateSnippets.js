f6e61f5ec8e708aca959df8433aff0bb8ff124ba
Xun.Chen <Xun.Chen@target.com>
2018-03-27 19:12:21 -0700

    added quote type setting feature

--- src/generateSnippets.js
+++ src/generateSnippets.js
@@ -4,30 +4,32 @@ var Mustache = require('mustache');
 var path = require('path');
 var fs = require('fs');
 
-const generateSnippets = (semi) => {
+const generateSnippets = (semi,quote) => {
   const templateData = fs.readdirSync(path.resolve(__dirname, 'templates')).forEach(
     file => {
       generateSnippet(
         path.resolve(__dirname, `templates/${file}`),
         path.resolve(__dirname, `../snippets/${file}`),
-        semi
+        semi,
+        quote
       );
     }
   );
 }
 
-const generateSnippet = (templateFile, outputFile, semi) => {
+const generateSnippet = (templateFile, outputFile, semi, quote) => {
   const templateData = fs.readFileSync(templateFile, 'utf-8');
-  const templateVariables = collectTemplateVariables(semi);
+  const templateVariables = collectTemplateVariables(semi,quote);
   const output = Mustache.render(templateData, templateVariables);
 
   fs.writeFile(outputFile, output, () => {});
 }
 
-const collectTemplateVariables = (semi) => {
+const collectTemplateVariables = (semi,quote) => {
   return {
     docsURL: "https://facebook.github.io/jest/docs",
     expressionTerminator: semi ? ';' : '',
+    quoteType: quote === '"' ? '\\"' : quote,
     label: "<span style='color:#99424f'>Jest</span>"
   };
 }
